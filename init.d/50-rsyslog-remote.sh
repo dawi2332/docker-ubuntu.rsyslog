d71360968b68ae0d2deceb447eb59f7b45636f1c
dawi2332 <dawi2332@gmail.com>
2015-02-21 16:20:15 +0100

    No longer deletes rsyslog default config.

--- init.d/50-rsyslog-remote.sh
+++ init.d/50-rsyslog-remote.sh
@@ -5,6 +5,4 @@ RSYSLOG_REMOTE=`etcdctl get /x44.email/services/rsyslog/remotehost`
 if [ ! -z $RSYSLOG_REMOTE ]; then
     echo "rsyslog remote host: $RSYSLOG_REMOTE"
     echo "*.* $RSYSLOG_REMOTE" > /etc/rsyslog.d/50-remote.conf
-
-    rm /etc/rsyslog.d/50-default.conf
 fi
