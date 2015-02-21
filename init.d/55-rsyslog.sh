96717e21d10218b940bc7760ec4ecb0a214103da
dawi2332 <dawi2332@gmail.com>
2015-02-21 16:26:51 +0100

    modified rsyslog startup script.

--- init.d/55-rsyslog.sh
+++ init.d/55-rsyslog.sh
@@ -4,6 +4,8 @@
 sed 's/$KLogPermitNonKernelFacility on/#$KLogPermitNonKernelFacility on/' -i /etc/rsyslog.conf
 sed 's/$ModLoad imklog/#$ModLoad imklog/' -i /etc/rsyslog.conf
 
+sed 's#|/dev/xconsole#/dev/console#' -i /etc/rsyslog.d/50-default.conf
+
 echo '$PreserveFQDN on' >> /etc/rsyslog.conf
 
 service rsyslog start
