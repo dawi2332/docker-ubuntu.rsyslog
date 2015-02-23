548689a7da168a7e3b34cc9d932e0b351095b0ae
dawi2332 <dawi2332@gmail.com>
2015-02-23 21:51:42 +0100

    Added supervisor to image.

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM dawi2332/etcdctl:v0.4.6
 
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get update && apt-get install -y rsyslog rsyslog-gnutls
+RUN apt-get update && apt-get install -y rsyslog rsyslog-gnutls supervisor
 
 # copy rsyslog config files and papertrail.com TLS cert
 COPY assets/etc/rsyslog.d/ /etc/rsyslog.d/
 COPY assets/etc/papertrail-bundle.pem /etc/papertrail-bundle.pem
 
 COPY init.d /docker-entrypoint-init.d
+
+ONBUILD COPY assets/etc/supervisor /etc/supervisor
--- init.d/55-rsyslog.sh
+++ init.d/55-rsyslog.sh
@@ -4,8 +4,10 @@
 sed 's/$KLogPermitNonKernelFacility on/#$KLogPermitNonKernelFacility on/' -i /etc/rsyslog.conf
 sed 's/$ModLoad imklog/#$ModLoad imklog/' -i /etc/rsyslog.conf
 
-sed 's#|/dev/xconsole#/dev/console#' -i /etc/rsyslog.d/50-default.conf
+# disable dysfunctional logging to /dev/xconsole
+sed 's/^\(daemon\.\*;mail\.\*;\\\)/#\1/' -i /etc/rsyslog.d/50-default.conf
+sed 's/^\(\s*news.err;\\\)/#\1/' -i /etc/rsyslog.d/50-default.conf
+sed 's/^\(\s*\*\.=debug;\*\.=info;\\\)/#\1/' -i /etc/rsyslog.d/50-default.conf
+sed 's!^\(\s*\*\.=notice;\*\.=warn\s*\|/dev/xconsole\)!#\1!' -i /etc/rsyslog.d/50-default.conf
 
 echo '$PreserveFQDN on' >> /etc/rsyslog.conf
-
-service rsyslog start
